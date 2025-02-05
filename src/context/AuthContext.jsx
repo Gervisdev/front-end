39d65f6257f34bfdc3896195fc46a389cbb39319
Gervisdev <gervisandjoua@gmail.com>
2025-02-05 14:18:33 +0100

    url changed api auth

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -16,7 +16,7 @@ const AuthContext = ({children}) => {
                 if(token){
 
     
-                const response = await axios.get('http://localhost:3000/api/auth/verify',{
+                const response = await axios.get('https://backend-api-mu-three.vercel.app/api/auth/verify',{
                     headers :{
                         "Authorization" : `Bearer ${token}`
                     }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
        
         try{
             const response = await axios.post(
-                "http://localhost:3000/api/auth/login",
+                "https://backend-api-mu-three.vercel.app/api/auth/login",
                 {email,password}
             );
             if(response.data.success){
