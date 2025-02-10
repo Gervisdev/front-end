b1b3a3f4d5bdcba99affde82e1df3e256702baf6
Gervisdev <gervisandjoua@gmail.com>
2025-02-10 11:03:58 +0100

    change style    components

--- src/components/departments/EditDepartment.jsx
+++ src/components/departments/EditDepartment.jsx
@@ -45,7 +45,7 @@ useEffect(() => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.put( `http://localhost:3000/api/department/${id}`,department, {
+      const response = await axios.put( `https://backend-api-orpin-seven.vercel.app/api/department/${id}`,department, {
           headers: {
             "Authorization": `Bearer ${localStorage.getItem("token")}`,
           },
@@ -60,7 +60,7 @@ useEffect(() => {
       }
     }  e.preventDefault();
     try {
-      const response = await axios.post( "http://localhost:3000/api/department/add",department, {
+      const response = await axios.post( "https://backend-api-orpin-seven.vercel.app/api/department/add",department, {
           headers: {
             "Authorization": `Bearer ${localStorage.getItem("token")}`,
           },
