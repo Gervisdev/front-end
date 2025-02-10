fb6c57a43033c75d8e24722e35f409cc7cdc1e20
Gervisdev <gervisandjoua@gmail.com>
2025-02-10 13:44:34 +0100

    useRef removed  components

--- src/components/EmployeeDashboard/Sidebar.jsx
+++ src/components/EmployeeDashboard/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import { FaBuilding, FaCogs, FaTachometerAlt, FaUsers } from 'react-icons/fa'
 import {NavLink} from 'react-router-dom'
 
