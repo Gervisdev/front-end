6ba1949f5bba7c45d366a66c72a8a5e99931d2da
Gervisdev <gervisandjoua@gmail.com>
2025-02-26 20:32:22 +0000

    deleteEmployee added

--- src/utils/EmployeeHelper.jsx
+++ src/utils/EmployeeHelper.jsx
@@ -64,6 +64,25 @@ export const columns = [
     
     const isAdmin = user?.role==="admin"//vérification si l'utilisateur est un admin//
 
+    const handleDelete = async () => {
+      if (!window.confirm("Voulez-vous vraiment supprimer cet employé ?")) {
+          return;
+      }
+  
+      try {
+          await axios.delete(`http://localhost:3000/api/employee/${Id}`, {
+              headers: {
+                  Authorization: `Bearer ${localStorage.getItem("token")}`,
+              },
+          });
+  
+          alert("Employé supprimé avec succès !");
+          window.location.reload(); // Recharge la page après suppression
+      } catch (error) {
+          console.error("Erreur lors de la suppression :", error);
+          alert(error.response?.data?.error || "Une erreur est survenue lors de la suppression.");
+      }
+  };
     
 
     return  (
@@ -90,6 +109,7 @@ export const columns = [
                 </button>
                 
                 <button className="px-3 py-1 text-white bg-red-600"
+                onClick={handleDelete}
                 >Supprimer
                 </button>
                 </>
