12488bc5e9129685eb6df600e674bd07335f0549
Gervisdev <gervisandjoua@gmail.com>
2025-02-28 07:48:56 +0000

    url changed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
        
         try{
             const response = await axios.post(
-                "https://backend-api-orpin-seven.vercel.app/api/auth/login",
+                "https://annuaire-api-olive.vercel.app/api/auth/login",
                 {email,password}
             );
             if(response.data.success){
