efa88e9b79d16e6010df9685fc6efcfa00fa2f50
Gervisdev <gervisandjoua@gmail.com>
2025-02-28 18:33:50 +0000

    url changed

--- src/components/dashboard/AdminSummary.jsx
+++ src/components/dashboard/AdminSummary.jsx
@@ -9,7 +9,7 @@ const AdminSummary = () => {
   useEffect(() => {
     const fetchSummary = async () => {
       try{
-        const summary = await axios.get('https://backend-api-orpin-seven.vercel.app/api/dashboard/summary',{
+        const summary = await axios.get('https://annuaire-api-olive.vercel.app/api/dashboard/summary',{
           headers:{
            " Authorization" : `Bearer ${localStorage.getItem('token')}`
           }
